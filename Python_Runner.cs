58d98839fe07087e90f0246df55680c38ba2b2c6
Sasan Sharifipour <sasansharifipour@gmail.com>
2020-06-23 12:29:07 +0430

    Remove comment in Python_Runner Class

--- Python_Runner.cs
+++ Python_Runner.cs
@@ -9,9 +9,9 @@ namespace Run_Python
 {
     public class Python_Runner
     {
-        public string python_path { get; set; } = @"C:\Users\admin\AppData\Local\Programs\Python\Python36\python.exe";
+        public string python_path { get; set; } = "";
         
-        public string script_path { get; set; } = @"E:\Deep Learning\Understanding Simple Speech Commands\SpeechRecognition\from_file.py";
+        public string script_path { get; set; } = "";
 
         public List<string> arguments { get; set; } = new List<string>();
 
@@ -33,29 +33,21 @@ namespace Run_Python
         }
 
         public void run()
-        {
-            // 1) Create Process Info
+        {            
             var psi = new ProcessStartInfo();
             psi.FileName = python_path;
 
-            // 2) Create Script
-            //var script = @"E:\Deep Learning\Understanding Simple Speech Commands\SpeechRecognition\from_file.py";
-            //var start = "2019_10_30";
-            psi.Arguments = generate_argument_command(); //$"\"{script_path}\"";
+            psi.Arguments = generate_argument_command();
 
-            // 3) Process Configuration
             psi.UseShellExecute = false;
             psi.CreateNoWindow = true;
             psi.RedirectStandardOutput = true;
             psi.RedirectStandardError = true;
 
-            // 4) Execute process and get output
-
             using (var process = Process.Start(psi))
             {
                 errors = process.StandardError.ReadToEnd();
                 results = process.StandardOutput.ReadToEnd();
-
             }
 
         }
